83a7cb01cee1b510b307c5419fc9192bb2f22e23
Gulfani Putra <gulfaniputra@gmail.com>
2025-11-06 16:46:36 +0800

    Update 'App.tsx' to use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
 import { HomePage } from './pages/HomePage';
+import { Header } from './components/Header';
+import { PostDetailPage } from './pages/PostDetailPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Header />
+
+      <Routes>
+        {/* Route for the homepage */}
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        {/* Route for the post detail page */}
+        <Route
+          path="/posts/:postId"
+          element={<PostDetailPage />}
+        />
+
+        {/* Add a 404 handler */}
+        <Route
+          path="*"
+          element={<div className="p-8 text-center">404 - Page Not Found</div>}
+        />
+      </Routes>
+    </Router>
+  );
 }
